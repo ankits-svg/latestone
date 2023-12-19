f81c8c5e7e03e9d2b9f02b454255d80e6f7a26a1
Ankit Sharma <ankits745@gmail.com>
2023-12-19 17:22:11 +0530

    front

--- front/src/Pages/Displaypage.jsx
+++ front/src/Pages/Displaypage.jsx
@@ -5,11 +5,20 @@ const DisplayPage = () => {
   return (
     <div className="containerStyle">
       <div className="leftDivStyle">
+        <div className="template">
+            <img src="https://github-production-user-asset-6210df.s3.amazonaws.com/103572350/291574948-3bd5cd5d-3ac1-4d23-a1cb-40482041c7fc.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231219%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231219T114025Z&X-Amz-Expires=300&X-Amz-Signature=89808a26e0dcc5bce9af22a0a15b0271746c98583ad5845f9f6f5c95c0e2698c&X-Amz-SignedHeaders=host&actor_id=103572350&key_id=0&repo_id=497514745" alt="temp" />
+        </div>
+
         <div className="contentStyle">
+          <div>
           <h1>Your Content Here</h1>
           <p>Additional text or components can go here.</p>
+          </div>
         </div>
       </div>
+      
+      
+      
       <div className="rightDivStyle">
         <h1>Right Div (30%)</h1>
         <div>
